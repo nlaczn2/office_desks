e4dfd45a857618b47838b1706ea2c2cf9e28ebaa
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-08 14:16:05 -0500

    move objects

---
+++ Project2/Assets/models Nicole/playSong.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSong : MonoBehaviour
+{
+
+    public AudioClip musicClip;
+    public AudioSource musicSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+            if (collision.gameObject.tag == "leftC" || collision.gameObject.tag == "rightC")
+            {
+            if (!musicSource.isPlaying)
+            {
+                musicSource.Play();
+            }
+
+            if (musicSource.isPlaying)
+            {
+                musicSource.Stop();
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+    }
+}
