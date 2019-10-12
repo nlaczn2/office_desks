876dc0faf80536a38c33d47da5d4a6a4a4245623
divay pandey <divay.pandey@outlook.com>
2019-10-12 15:13:34 -0500

    Signed-off-by: divay pandey <divay.pandey@outlook.com>

---
+++ Project2/Assets/models_Divay/on_off.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class on_off : MonoBehaviour
+{
+    public GameObject yellow_light;
+
+    public void toggleLight()
+    {
+        if(yellow_light.activeInHierarchy)
+        {
+            yellow_light.SetActive(false);
+        }
+        else if(!yellow_light.activeInHierarchy)
+        {
+            yellow_light.SetActive(true);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
