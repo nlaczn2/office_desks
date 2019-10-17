49e79dc6b8152db2376a99fcfee15faa06eb6e53
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-17 16:31:44 -0500

    part B done

---
+++ Project2/Assets/models Nicole/typer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class typer : MonoBehaviour
+{
+    public GameObject tallLightOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void typeing() {
+        if (!tallLightOn.activeInHierarchy)
+        {
+            tallLightOn.SetActive(true);
+        }
+    }
+}
