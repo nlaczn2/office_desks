156f104d50853fd081c8020b30d487f8dd71487b
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-18 18:50:28 -0500

    dead spider

---
+++ Project2/Assets/models Nicole/flash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class flash : MonoBehaviour
+{
+    public GameObject tallLightOn;
+    public GameObject tallLightOff;
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            //other stuff
+
+    }
+
+}
