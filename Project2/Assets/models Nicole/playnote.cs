baaf94f73223240e5c003fceadea7d206133a7f3
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-19 00:10:17 -0500

    part A items

---
+++ Project2/Assets/models Nicole/playnote.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playnote : MonoBehaviour
+{
+    public GameObject tallLightOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void somefn()
+    {
+        if (tallLightOn.activeInHierarchy)
+        {
+            tallLightOn.SetActive(false);
+        }
+        else if (!tallLightOn.activeInHierarchy)
+        {
+            tallLightOn.SetActive(true);
+        }
+    }
+}
