90e638f05db482e9b5621a1da726cffb1ff3b610
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-19 00:55:58 -0500

    resizeing

---
+++ Project2/Assets/models Nicole/shrinkScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrinkScript : MonoBehaviour
+{
+    public GameObject room;
+    public GameObject Person;
+    public float x = 0.3f;
+    public float y = 0.3f;
+    public float z = 0.3f;
+    public float x1 = 0.3f;
+    public float y1 = 0.3f;
+    public float z1 = 0.3f;
+
+    public void shrink() {
+
+        room.transform.localScale = new Vector3(x, y, z);
+        Person.transform.localPosition= new Vector3(x1, y1, z1);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
