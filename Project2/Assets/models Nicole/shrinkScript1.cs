e6bdf9acc256d86484f8f8c52825bd31878725ff
Nicole Laczny <nicolelaczny@gmail.com>
2019-10-19 14:10:52 -0500

    shrink grow + physics+ text

--- Project2/Assets/models Nicole/shrinkScript1.cs
+++ Project2/Assets/models Nicole/shrinkScript1.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class shrinkScript1 : MonoBehaviour
 {
+    public int small = 0;
+    public int normal = 0;
+    public int big = 0;
+
+    public GameObject officeLoud;
+    public GameObject officeSoft;
+    public GameObject officeNorm;
+
+
+    public GameObject talkLoud;
+    public GameObject talkSoft;
+    public GameObject talkNorm;
+
 
     public GameObject Person;
     public float x = 0.3f;
@@ -13,10 +26,37 @@ public class shrinkScript1 : MonoBehaviour
     public float y1 = 0.3f;
     public float z1 = 0.3f;
 
+
+
     public void shrink() {
 
         Person.transform.localScale = new Vector3(x, y, z);
         Person.transform.localPosition= new Vector3(x1, y1, z1);
+
+        if (small == 1) {
+            officeLoud.SetActive(true);
+            talkLoud.SetActive(true);
+            officeSoft.SetActive(false);
+            talkSoft.SetActive(false);
+            officeNorm.SetActive(false);
+            talkNorm.SetActive(false);
+        }
+        else if (normal == 1) {
+            officeLoud.SetActive(false);
+            talkLoud.SetActive(false);
+            officeSoft.SetActive(false);
+            talkSoft.SetActive(false);
+            officeNorm.SetActive(true);
+            talkNorm.SetActive(true);
+        }
+        else if (big == 1) {
+            officeLoud.SetActive(false);
+            talkLoud.SetActive(false);
+            officeSoft.SetActive(true);
+            talkSoft.SetActive(true);
+            officeNorm.SetActive(false);
+            talkNorm.SetActive(false);
+        }
 
     }
     // Start is called before the first frame update
